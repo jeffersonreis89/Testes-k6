eccd9595451d0d0638c92b0251aab87c922cdd8d
Jefferson Reis <78965968+jeffersonreis89@users.noreply.github.com>
2022-02-03 11:03:19 -0300

    added rate

--- tests/06-rate.js
+++ tests/06-rate.js
@@ -1,8 +1,14 @@
 import { check } from "k6";
 import http from "k6/http";
-import { Rate } from "k6/metrics";
+import { Rate, Trend } from "k6/metrics";
 
 export let errorRate = new Rate('errors')
+
+
+var getApiTrend = new Trend ("TREND_Get_Api_Duration");
+var getApiTrendWaiting = new Trend ("TREND_Get_Api_Waiting");
+var googleGetApiTrend = new Trend ("TREND_Google_Get_Api_Duration");
+var googleGetApiTrendWaiting = new Trend ("TREND_Google_Get_Api_Waiting");
 
 export let options = {
     thresholds:{
@@ -12,7 +18,6 @@ export let options = {
 
 export default function(){
     let res = http.get('https://run.mocky.io/v3/f529e682-98ae-4aae-b8a9-15f15900d0c5');
-    // let res = http.get('https://webhook.site/aa09534e-ca8d-44dc-af32-035c48831abc');
 
     console.log(`response body length ${res.body.length} for VU =  ${__VU} ITERA = ${__ITER}`)
      
@@ -23,8 +28,17 @@ export default function(){
     errorRate.add(!check1);
 
     const check2 = check(res, {
-        'body size is 45 bytes :' : (r) => r.body.length == 5,
+        'body size is 45 bytes :' : (r) => r.body.length == 45,
     });
 
     errorRate.add(!check2);
+
+    getApiTrend.add(res.timings.duration)
+    getApiTrendWaiting.add(res.timings.waiting)
+
+
+    const googleResponse = http.get('https://www.google.com/')
+    googleGetApiTrend.add(googleResponse.timings.duration)
+    gooleGetApiTrendWaiting.add(googleResponse.timings.waiting)
+
 }
