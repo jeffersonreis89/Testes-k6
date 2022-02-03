d53ac9317378cbee97c10ce997827f062aeaccaa
Jefferson Reis <78965968+jeffersonreis89@users.noreply.github.com>
2022-02-03 11:27:58 -0300

    adicionando o retryTrend

--- tests/18-counter-retry.js
+++ tests/18-counter-retry.js
@@ -2,6 +2,9 @@ import { Counter } from "k6/metrics";
 import http from "k6/http";
 import { sleep } from "k6";
 
+import { Trend } from "k6/metrics";
+var retryTrend = new Trend ('GETAPI_MAX_RETRY_TREND')
+
 var retryCounter = new Counter("GetApi_Max_Retry");
 
 export default function () {
@@ -9,6 +12,7 @@ export default function () {
   retryCounter.add(1);
   for (var retries = 5; retries > 0; retries++) {
       var numberOfAttempts = maxAttemps - retries + 1
+      retryTrend.add(numberOfAttempts)
     const response = http.get(
       "https://run.mocky.io/v3/4b6daa06-74bb-4d34-b297-7eb798e2e95e"
     );
